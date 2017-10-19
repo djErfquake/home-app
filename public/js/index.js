e985fe67497272e03eeba5aa8be40d7c3242de4f
Calvin LeVally <clevally@roto.com>
2017-10-19 13:52:43 -0400

    temperature and icon

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,15 @@
 let lat = "39.908702";
 let lon = "-83.114147";
 let darkskyKey = "c98746a2df07c27aa2a80d00f4e659a8";
+let clockInterval, weatherInterval;
 
 $(document).ready(() => {
 
-  updateWeather();
+  updateClock();
+  clockInterval = setInterval(updateClock, 1000); // update every second
 
+  updateWeather();
+  weatherInterval = setInterval(updateWeather, 900000); // update every 15 minutes
 
 });
 
@@ -14,7 +18,8 @@ let updateWeather = () => {
 
       // success
       console.log("darksky data", data);
-      $('.temperature').html(data.currently.temperature + "°");
+      $('.temperature-number').html(Math.round(data.currently.temperature) + "°");
+      setWeatherIcon(data.currently.icon);
 
     }, error: function (data, textStatus, errorThrown) {
 
@@ -24,4 +29,62 @@ let updateWeather = () => {
 			console.warn("Chrome Error: " + errorThrown.Message);
     }
   })
+};
+
+let updateClock = () => {
+
+
+};
+
+let setWeatherIcon = (weatherIcon) => {
+
+  // TODO: add support for differentiating between day and night
+  // https://erikflowers.github.io/weather-icons/
+
+  console.log("setting weather icon to [" + weatherIcon + "]");
+  switch (weatherIcon) {
+    case "clear-day":
+      $('.temperature-icon').addClass("wi-day-sunny");
+      break;
+    case "clear-night":
+      $('.temperature-icon').addClass("wi-night-clear");
+      break;
+    case "rain":
+      $('.temperature-icon').addClass("wi-rain");
+      break;
+    case "snow":
+      $('.temperature-icon').addClass("wi-snow");
+      break;
+    case "sleet":
+      $('.temperature-icon').addClass("wi-sleet");
+      break;
+    case "wind":
+      $('.temperature-icon').addClass("wi-windy");
+      break;
+    case "fog":
+      $('.temperature-icon').addClass("wi-fog");
+      break;
+    case "cloudy":
+      $('.temperature-icon').addClass("wi-cloudy");
+      break;
+    case "partly-cloudy-day":
+      $('.temperature-icon').addClass("wi-day-cloudy");
+      break;
+    case "partly-cloudy-night":
+      $('.temperature-icon').addClass("wi-night-alt-cloudy");
+      break;
+    case "hail":
+      $('.temperature-icon').addClass("wi-hail");
+      break;
+    case "thunderstorm":
+      $('.temperature-icon').addClass("wi-thunderstorm");
+      break;
+    case "tornado":
+      $('.temperature-icon').addClass("wi-tornado");
+      break;
+    default:
+    $('.temperature-icon').addClass("wi-thermometer");
+    break;
+
+  }
 };
